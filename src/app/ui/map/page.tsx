033081486ebeb51e7795741f0f81df34dc2ec48c
yeiid <yeifranhernandez16@gmail.com>
2024-03-10 07:26:33 -0500

    errorMap

--- src/app/ui/map/page.tsx
+++ src/app/ui/map/page.tsx
@@ -1,10 +1,13 @@
-import Map from '@/components/Map'
+// import Map from '@/components/Map'
+
+
+
 export default function Page() {
   return (
 <>
     <div className='flex item-center py-5'>
     <h2>Localizacion actaul</h2>
-      <Map/>
+      {/* <Map/> */}
     </div>
 
     </>
