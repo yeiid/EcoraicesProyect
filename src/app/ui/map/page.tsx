362f65596baadb6798dd38777e3fe40773f53039
yeiid <yeifranhernandez16@gmail.com>
2024-03-12 11:57:35 -0500

    DynamicError

--- src/app/ui/map/page.tsx
+++ src/app/ui/map/page.tsx
@@ -1,11 +1,15 @@
-import Map from '@/components/Map'
 
+import dynamic from 'next/dynamic';
+
+const MapComponent = dynamic(() => import('@/components/Map'), {
+  ssr: false,
+});
 export default function Page() {
   return (
 <>
     <div className='flex item-center py-5'>
     <h2>Localizacion actaul</h2>
-      <Map/>
+      <MapComponent />
     </div>
 
     </>
